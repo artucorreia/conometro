dc1a4f4a5794886a32125e45f1aa9168e464583a
Arthur Correia <115330684+artucorreia@users.noreply.github.com>
2022-11-11 15:19:56 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 const timer = window.document.querySelector('div#timer');
 const btns = window.document.querySelector('div#btns');
+const intervalId = null
 
 const button = event => main[event.target.id]();
+
+let time = {
+    'ms': 0,
+    'seg': 0,
+    'min': 0,
+    'h': 0,
+}
+
+const start = () => {
+    time.ms++    
+    timer.innerHTML = `0${time.h} : 0${time.min} : 0${time.seg}.${time.ms}`
+}
 
 // main
 const main = {
     'save':  () => alert('save ok'),
-    'start': () => alert('start ok'),
+    'start': () => intervalId = setInterval(start, 1),
     'pause': () => alert('pause ok')
 };
 
